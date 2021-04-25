4f16a17a5b4e07310809a1cdd846f1ef8988cad7
takurofukuta <mojuraspax@icloud.com>
2021-04-25 18:49:49 +0900

    マイグレーションファイルのユーザー名、苗字、名前、研究室配属年度カラムにnull: falseを追加

--- db/migrate/20210425092307_devise_create_users.rb
+++ db/migrate/20210425092307_devise_create_users.rb
@@ -32,10 +32,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-      t.string :user_name
-      t.string :last_name
-      t.string :first_name
-      t.integer :assignment_year
+      t.string :user_name, null: false
+      t.string :last_name, null: false
+      t.string :first_name, null: false
+      t.integer :assignment_year, null: false
 
       t.timestamps null: false
     end
