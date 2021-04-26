412d6b65acd194073c2835a71c1a9707feebd51b
takurofukuta <mojuraspax@icloud.com>
2021-04-26 19:54:37 +0900

    Equipmentモデルのファイルにバリデーションを追加

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -2,6 +2,12 @@ class Equipment < ApplicationRecord
   has_one :lending, dependent: :destroy
   belongs_to :registered_user, optional: true, class_name: "User"
 
+  validates :genre, presence: true
+  validates :lab_equipment_name, presence: true
+  validates :maker_name, presence: true
+  validates :product_name, presence: true
+  validates :purchase_year, presence: true
+
   enum genre: {
     pc: 0,
     note_pc: 1,
