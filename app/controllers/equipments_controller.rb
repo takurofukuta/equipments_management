be4395a22e5afcdb8646d4442b73415fd2107283
takurofukuta <mojuraspax@icloud.com>
2021-04-26 22:23:30 +0900

    equipmentsのルーティングとequipments#indexの作成

--- app/controllers/equipments_controller.rb
+++ app/controllers/equipments_controller.rb
@@ -1,10 +1,29 @@
 class EquipmentsController < ApplicationController
   def index
-  end
-
-  def edit
+    @equipments = Equipment.all
+    @equipment = Equipment.new
   end
 
   def show
+    @equipment = Equipment.find(params[:id])
+  end
+
+  def edit
+    @equipment = Equipment.find(params[:id])
+  end
+
+  def create
+  end
+
+  def update
+    equipment = Equipment.find(params[:id])
+    equipment.update!(equipment_params)
+    redirect_to equipment
+  end
+
+  def destroy
+    equipment = Equipment.find(params[:id])
+    equipment.destroy!
+    redirect_to root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  get 'operation_histories/index'
-  get 'lendings/index'
-  get 'lendings/history'
-  get 'equipments/index'
-  get 'equipments/edit'
-  get 'equipments/show'
+  root to: "equipments#index"
+  resources :equipments, expect: :new
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
