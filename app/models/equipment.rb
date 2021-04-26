4abe0d0c9d79ebf5344559adc80374083d93a778
takurofukuta <mojuraspax@icloud.com>
2021-04-26 19:41:06 +0900

    Equipmentモデルのgenreのenumを設定

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -1,4 +1,13 @@
 class Equipment < ApplicationRecord
   has_one :lending, dependent: :destroy
   belongs_to :registered_user, optional: true, class_name: "User"
+
+  enum genre: {
+    pc: 0,
+    note_pc: 1,
+    tera: 2,
+    camera: 3,
+    experiment: 4,
+    others: 5,
+  }
 end
