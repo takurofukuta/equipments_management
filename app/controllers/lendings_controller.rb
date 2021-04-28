889acb8a9fd43e7cf8cf7cc2b43a9bdc8889b0ba
takurofukuta <mojuraspax@icloud.com>
2021-04-28 16:23:27 +0900

    lendings#indexと貸出状況表示ページの作成

--- app/controllers/lendings_controller.rb
+++ app/controllers/lendings_controller.rb
@@ -1,8 +1,10 @@
 class LendingsController < ApplicationController
   before_action :authenticate_user!
+
   def index
+    @lendings = Lending.all
   end
 
-  def history
+  def lendings_history
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   root to: "equipments#index"
   resources :equipments, expect: :new
+  get "/lendings", to: "lendings#index"
+  get "/lendings/history", to: "lendings#lendings_history"
+  post "/lendings/:id", to: "lendings#lending"
+  patch "/lendings/:id", to: "lendings#return"
+
   devise_for :users
 end
