eb36e6ba287b418c2b1045288db7e6592a0e638c
takurofukuta <mojuraspax@icloud.com>
2021-04-30 18:23:47 +0900

    備品一覧表示ページをデザイン

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,15 @@ user1 = User.create!(user_name: "test1", last_name: "test", first_name: "太郎"
 user2 = User.create!(user_name: "test2", last_name: "test", first_name: "二郎", assignment_year: 2016, password: "password")
 user3 = User.create!(user_name: "test3", last_name: "test", first_name: "三郎", assignment_year: 2016, password: "password")
 10.times do |i|
-Equipment.create!(genre: rand(0..5), lab_equipment_name: Faker::Device.serial, maker_name: Faker::Device.manufacturer, product_name: Faker::Device.model_name, purchase_year: rand(1990..2020), asset_num: Faker::Device.serial,
+Equipment.create!(genre: rand(0..5), lab_equipment_name: "KJ-#{Faker::Device.manufacturer}-XXXX-XX", maker_name: Faker::Device.manufacturer, product_name: Faker::Device.model_name, purchase_year: rand(1990..2020), asset_num: "XXXX-XXXX-XXXX",
 price: rand(1000..500000), remarks: "test", registered_user_id: user1.id )
 end
 10.times do |i|
-Equipment.create!(genre: rand(0..5), lab_equipment_name: Faker::Device.serial, maker_name: Faker::Device.manufacturer, product_name: Faker::Device.model_name, purchase_year: rand(1990..2020), asset_num: Faker::Device.serial,
+Equipment.create!(genre: rand(0..5), lab_equipment_name: "KJ-#{Faker::Device.manufacturer}-XXXX-XX", maker_name: Faker::Device.manufacturer, product_name: Faker::Device.model_name, purchase_year: rand(1990..2020), asset_num: "XXXX-XXXX-XXXX",
 price: rand(1000..500000), remarks: "test", registered_user_id: user2.id )
 end
 10.times do |i|
-Equipment.create!(genre: rand(0..5), lab_equipment_name: Faker::Device.serial, maker_name: Faker::Device.manufacturer, product_name: Faker::Device.model_name, purchase_year: rand(1990..2020), asset_num: Faker::Device.serial,
+Equipment.create!(genre: rand(0..5), lab_equipment_name: "KJ-#{Faker::Device.manufacturer}-XXXX-XX", maker_name: Faker::Device.manufacturer, product_name: Faker::Device.model_name, purchase_year: rand(1990..2020), asset_num: "XXXX-XXXX-XXXX",
 price: rand(1000..500000), remarks: "test", registered_user_id: user3.id )
 end
 puts "初期データの作成完了"
