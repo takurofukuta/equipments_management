039adf102452330bfe1ef9c853c78ead94b5fbba
takurofukuta <mojuraspax@icloud.com>
2021-05-01 00:32:07 +0900

    備品データ編集ページのデザイン

--- app/models/operation_history.rb
+++ app/models/operation_history.rb
@@ -2,8 +2,8 @@ class OperationHistory < ApplicationRecord
   belongs_to :operated_user, class_name: "User", foreign_key: :operated_user_id
 
   class << self
-    def create_log(operated_user_id, lab_equipment_name, content)
-      OperationHistory.create!(operated_user_id: operated_user_id, lab_equipment_name: lab_equipment_name, content: content)
+    def create_log(operated_user_id, object, content)
+      OperationHistory.create!(operated_user_id: operated_user_id, object: object, content: content)
     end
   end
 
