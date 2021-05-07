4e876c7162d65c218303890e7e16cb662396173b
takurofukuta <mojuraspax@icloud.com>
2021-05-08 03:05:03 +0900

    seedファイルを修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,9 @@ require "faker"
 
 User.destroy_all
 Equipment.destroy_all
-user1 = User.create!(user_name: "test1", last_name: "test", first_name: "太郎", assignment_year: 2016, password: "password")
-user2 = User.create!(user_name: "test2", last_name: "test", first_name: "二郎", assignment_year: 2016, password: "password")
-user3 = User.create!(user_name: "test3", last_name: "test", first_name: "三郎", assignment_year: 2016, password: "password")
+user1 = User.create!(user_name: "tarotanaka", last_name: "田中", first_name: "太郎", assignment_year: 2016, password: "password")
+user2 = User.create!(user_name: "jirosato", last_name: "佐藤", first_name: "二郎", assignment_year: 2017, password: "password")
+user3 = User.create!(user_name: "saburoito", last_name: "伊藤", first_name: "三郎", assignment_year: 2018, password: "password")
 10.times do |i|
 Equipment.create!(genre: rand(0..5), lab_equipment_name: "KJ-#{Faker::Device.manufacturer}-XXXX-XX", maker_name: Faker::Device.manufacturer, product_name: Faker::Device.model_name, purchase_year: rand(1990..2020), asset_num: "XXXX-XXXX-XXXX",
 price: rand(1000..500000), remarks: "test", registered_user_id: user1.id )
