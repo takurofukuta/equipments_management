b46de2dbfebc368338b6fc03832fd645a6c446bb
takurofukuta <mojuraspax@icloud.com>
2021-05-08 02:18:55 +0900

    アカウント編集情報をデータベースに登録できるように設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :assignment_year])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:last_name, :first_name, :assignment_year])
   end
 end
