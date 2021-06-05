cc3106a5d5ccb8573307f9489a6a464f654a8dab
takurofukuta <mojuraspax@icloud.com>
2021-06-05 18:35:26 +0900

    factories/user.rbにテストで使用するデータをFakerを用いて修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,10 @@
 FactoryBot.define do
   factory :user do
-    user_name { "satou_maruo"}
-    last_name { "佐藤" }
-    first_name { "丸男" }
+    user_name { Faker::Code.unique.npi }
+    last_name { Faker::Name.last_name }
+    first_name { Faker::Name.first_name }
     assignment_year { 2016 }
     password { "password "}
+    admin { false }
   end
 end
