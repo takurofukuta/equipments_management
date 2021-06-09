f7005ca0299eb03e78a9a86b5787576f92004fa1
takurofukuta <mojuraspax@icloud.com>
2021-06-09 14:41:13 +0900

    Userモデルスペックにテストを追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,1 +1,62 @@
 require "rails_helper"
+
+RSpec.describe User, type: :model do
+  describe "バリデーションのチェック" do
+
+    context "データが条件を満たす時" do
+      let(:user) { build(:user) }
+      it "保存できる" do
+        expect(user.valid?).to eq true
+      end
+    end
+    context "user_nameが空の場合" do
+      let(:user) { build(:user, user_name: "") }
+      it "エラーが発生し、エラーメッセージが表示される" do
+        expect(user.valid?).to eq false
+        expect(user.errors.messages).to include "を入力してください"
+      end
+    end
+    context "" do
+      
+    end
+    context "" do
+      
+    end
+    context "" do
+      
+    end
+    context "" do
+      
+    end
+    context "" do
+      
+    end
+    context "" do
+      
+    end
+    context "" do
+      
+    end
+    context "" do
+      
+    end
+    context "" do
+      
+    end
+    context "" do
+      
+    end
+    context "" do
+      
+    end
+    context "" do
+      
+    end
+    context "" do
+      
+    end
+    context "" do
+      
+    end
+  end
+end
